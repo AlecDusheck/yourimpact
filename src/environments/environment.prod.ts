1a1cee2c1c20154eed84aa9d350b8f26bd197ebc
alecdusheck <alec@simplyalec.com>
2020-11-19 21:50:56 -0600

    Add message explaining this API key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
+  // This is NOT secret. The maps API key needs to be exposed publicly anyways and is 1) restricted to the JS Maps API and
+  // 2) only works on *.simplyalec.com/* domains
   mapsApiKey: 'AIzaSyCR-zOfXNfwo_ifWnW6V4of456QelO9Qv8'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,8 @@
 
 export const environment = {
   production: false,
+  // This is NOT secret. The maps API key needs to be exposed publicly anyways and is 1) restricted to the JS Maps API and
+  // 2) only works on *.simplyalec.com/* domains
   mapsApiKey: 'AIzaSyCR-zOfXNfwo_ifWnW6V4of456QelO9Qv8'
 };
 
